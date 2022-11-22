fc4169202f4ef8f52306b0ec5ebb442179d31b6f
Tácio Soares <taciofsoares@gmail.com>
2022-11-22 16:21:07 -0300

    ww

--- JSalura01/cap4/introducao-javascript/js/filtro.js
+++ JSalura01/cap4/introducao-javascript/js/filtro.js
@@ -3,8 +3,8 @@ campoFiltro.addEventListener("input", function(){
     console.log(this.value)
     var pacientes = document.querySelectorAll('.paciente')
     for(var i = 0; i < pacientes.length; i++) {
-        var paciente = pacientes[i]
-        var nome = paciente.querySelector('.info-nome').textContent
-        console.log(nome)
+        var cara = pacientes[i]
+        var nome = cara.querySelector('.info-nome').textContent
+        console.log(cara)
     }
 })
