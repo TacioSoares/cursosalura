0a854c73f958aacf5fd8dffd742d118bf5403614
Tácio Soares <taciofsoares@gmail.com>
2022-11-24 11:39:40 -0300

    expreg

--- JSalura01/cap4/introducao-javascript/js/filtro.js
+++ JSalura01/cap4/introducao-javascript/js/filtro.js
@@ -1,10 +1,21 @@
 var campoFiltro = document.querySelector("#ifiltro")
 campoFiltro.addEventListener("input", function(){
-    console.log(this.value)
     var pacientes = document.querySelectorAll('.paciente')
-    for(var i = 0; i < pacientes.length; i++) {
-        var cara = pacientes[i]
-        var nome = cara.querySelector('.info-nome').textContent
-        console.log(cara)
+    if (campoFiltro.value.length > 0) {
+        for(var i = 0; i < pacientes.length; i++) {
+            var cara = pacientes[i]
+            var nome = cara.querySelector(".info-nome").innerText
+            var expressao = new RegExp(campoFiltro.value,"i")
+            if (!expressao.test(nome)) {
+                cara.classList.add('filtrado')    
+            } else {
+                cara.classList.remove('filtrado')
+            }
+        }
+    } else {
+        for(var i = 0; i < pacientes.length; i++) {
+            var cara = pacientes[i]
+            cara.classList.remove('filtrado')
+        }    
     }
 })
