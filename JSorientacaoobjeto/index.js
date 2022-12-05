403e24e1bac8a2291ba3c75ed6c941dccbeec9f8
Tácio Soares <taciofsoares@gmail.com>
2022-12-05 11:46:27 -0300

    95

--- JSorientacaoobjeto/index.js
+++ JSorientacaoobjeto/index.js
@@ -4,20 +4,12 @@ const cliente1 = new Cliente()
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-
 const contaCorrenteRicardo = new ContaCorrente()
 contaCorrenteRicardo.cliente = cliente1
 
 contaCorrenteAlice = new ContaCorrente()
-contaCorrenteAlice.cliente = cliente2
+contaCorrenteAlice.cliente = new Cliente()
+contaCorrenteAlice.cliente.nome = 'Alice'
+contaCorrenteAlice.cliente.cpf = 88822233309
 
 console.log(contaCorrenteAlice)
-console.log(contaCorrenteRicardo)
-
-contaCorrenteAlice.transferir(200,contaCorrenteRicardo)
-
-console.log(contaCorrenteAlice)
-console.log(contaCorrenteRicardo)
