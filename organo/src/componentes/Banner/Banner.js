7124659bed07a7ddebd9da9d966c458e98aa8d86
Tácio Soares <taciofsoares@gmail.com>
2022-12-14 16:07:59 -0300

    ok

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -1,7 +1,11 @@
+import './Banner.css'
+
 function Banner() {
     // JSX 
     return (
-        <img src="/imagens/banner.png" alt="banner principal da página do organo"></img>
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="banner principal da página do organo"></img>
+        </header>
     )
 }
 
