da35af23b8b812ce8f40e892db616e298968227f
Tácio Soares <taciofsoares@gmail.com>
2023-01-24 14:44:04 -0300

    20213

--- CRUD-com-JS/js-consumindo-dados-api-main/script.js
+++ CRUD-com-JS/js-consumindo-dados-api-main/script.js
@@ -1,6 +1,6 @@
 
-
-var cep = 18110382
+var incep = document.querySelector('#cep')
+incep.addEventListener('focusout', () => buscaEndereço(incep.value))
 async function buscaEndereço(cep) {
     try {
         var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
@@ -8,10 +8,29 @@ async function buscaEndereço(cep) {
         if (consultaCepJSON.erro) {
             throw Error ('CEP inválido')
         }
+        var campos = await pegaCampos()
+        await preencheCampos(consultaCepJSON,campos)
         console.log(consultaCepJSON)
     } catch (erro) {
-        console.log(erro)
+        window.alert('CEP Inválido')
+        camposVazios()
     }    
 }
 
-buscaEndereço(cep)
+function pegaCampos() {
+    var campos = [document.querySelector('#endereco'),document.querySelector('#bairro'),document.querySelector('#cidade'),document.querySelector('#estado')]
+    return campos
+}
+function preencheCampos(informacoes,campos) {
+    console.log(campos[0])
+    campos[0].value = informacoes.logradouro
+    campos[1].value = informacoes.bairro
+    campos[2].value = informacoes.localidade
+    campos[3].value = informacoes.uf
+    return campos
+}
+
+function camposVazios() {
+    var campos = [document.querySelector('#endereco'),document.querySelector('#bairro'),document.querySelector('#cidade'),document.querySelector('#estado')]
+
+}
